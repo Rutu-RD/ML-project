a32b03e6547a24642f808d541e9036e4f534e4ba
ruturaj desai <desairuturaj98@gmail.com>
2023-08-25 03:46:30 -0400

    added logging to exception file

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     #to get exeception at which line and what is it 
@@ -23,6 +23,16 @@ class customexception(Exception):
 
     def __str__(self):
         return self.error_message    
+
+
+
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise customexception(e,sys)
 
 
      
